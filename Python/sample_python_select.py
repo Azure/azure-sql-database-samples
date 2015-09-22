35e3598b72d06a5ae38885c06ffa6c621fa32ca3
Andrea Lam <andrela@microsoft.com>
2015-09-22 15:25:46 -0700

    Update sample_python_select.py

--- Python/sample_python_select.py
+++ Python/sample_python_select.py
@@ -11,7 +11,19 @@ cursor = conn.cursor()
 cursor.execute('SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;')
 row = cursor.fetchone()
 
-#Print results. Specify as many columns as you require
+#Print results from select statement.
 while row:
     print str(row[0]) + " " + str(row[1]) + " " + str(row[2])   
     row = cursor.fetchone()
+    
+#Execute an insert statement    
+cursor.execute("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES ('SQL Server Express', 'SQLEXPRESS', 0, 0, CURRENT_TIMESTAMP)")
+row = cursor.fetchone()
+
+#Print the ID of the inserted row. 
+while row:
+    print "Inserted Product ID : " +str(row[0])
+    row = cursor.fetchone()
+    
+    
+ 
