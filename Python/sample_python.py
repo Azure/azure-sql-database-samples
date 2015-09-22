1f68df4536e1df1cfe45257eec38d882a536e36f
Andrea Lam <andrela@microsoft.com>
2015-09-22 16:20:58 -0700

    Update sample_python.py

--- Python/sample_python.py
+++ Python/sample_python.py
@@ -7,6 +7,7 @@ conn = pymssql.connect(server='yourserver.database.windows.net', user='yourusern
 	password='yourpassword', database='AdventureWorks')
 cursor = conn.cursor()
 
+#SELECT
 #Execute a simple select statement. 
 cursor.execute('SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;')
 row = cursor.fetchone()
@@ -15,7 +16,8 @@ row = cursor.fetchone()
 while row:
     print str(row[0]) + " " + str(row[1]) + " " + str(row[2])   
     row = cursor.fetchone()
-    
+
+#INSERT    
 #Execute an insert statement    
 cursor.execute("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES ('SQL Server Express', 'SQLEXPRESS', 0, 0, CURRENT_TIMESTAMP)")
 row = cursor.fetchone()
