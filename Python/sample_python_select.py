b79d90baa7bc93da7a32ef783f897e2393868211
Andrea Lam <andrela@microsoft.com>
2015-09-22 14:36:11 -0700

    Update sample_python_select.py

--- Python/sample_python_select.py
+++ Python/sample_python_select.py
@@ -2,17 +2,16 @@
 import pymssql
 
 #Connect to your database. 
-#Replace server name, username, password, and database name with your credentials 
-conn = pymssql.connect(server='servername.database.windows.net', 
-	user='username@servername', password='password', 
-	database='databasename')
+#Replace server name, username, and password with your credentials    
+conn = pymssql.connect(server='yourserver.database.windows.net', user='yourusername@yourserver', 
+	password='yourpassword', database='AdventureWorks')
 cursor = conn.cursor()
 
 #Execute a simple select statement. 
-#Replace schema name and table name with your own
-cursor.execute('SELECT TOP 10 * FROM [schemaname].[tablename]')
+cursor.execute('SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;')
 row = cursor.fetchone()
+
+#Print results. Specify as many columns as you require
 while row:
-	#Print results. Specify as many columns as you require
     print str(row[0]) + " " + str(row[1]) + " " + str(row[2])   
     row = cursor.fetchone()
