03fac9f6cc11d0c21ed2630a4bc0f468596d3669
Meet Bhagdev <mebha@microsoft.com>
2015-09-22 16:44:49 -0700

    django app

--- Django/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^admin/', include(admin.site.urls)),
-)
