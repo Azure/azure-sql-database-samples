178353e6e562f4002ba2e426528c245d564f78aa
Andrea Lam <andrela@microsoft.com>
2015-09-24 11:30:37 -0700

    Update sample_ruby_mac.rb

--- Ruby/Mac OS X/sample_ruby_mac.rb
+++ Ruby/Mac OS X/sample_ruby_mac.rb
@@ -16,3 +16,27 @@ results.each do |row|
 puts row 
 end 
 
+#INSERT
+#Make sure you have followed the recommended settings for using TinyTDS with Azure
+results = client.execute("SET ANSI_NULLS ON")
+results = client.execute("SET CURSOR_CLOSE_ON_COMMIT OFF")
+results = client.execute("SET ANSI_NULL_DFLT_ON ON")
+results = client.execute("SET IMPLICIT_TRANSACTIONS OFF")
+results = client.execute("SET ANSI_PADDING ON")
+results = client.execute("SET QUOTED_IDENTIFIER ON")
+results = client.execute("SET ANSI_WARNINGS ON")
+results = client.execute("SET CONCAT_NULL_YIELDS_NULL ON")
+
+#Configure date format to align with SQL Server datetime format
+require 'date'
+t = Time.now
+curr_date = t.strftime("%Y-%m-%d %H:%M:%S.%L") 
+
+#Execute an insert statement  
+results = client.execute("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) 
+OUTPUT INSERTED.ProductID VALUES ('SQL Server Express New', 'SQLEXPRESS New', 0, 0, '#{curr_date}' )")
+
+#Print the ID of the inserted row. 
+results.each do |row| 
+puts row
+end 
