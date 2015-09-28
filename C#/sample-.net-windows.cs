b284b66e4ab9824f8dcf468049d61706f694e4aa
Brian Gianforcaro <bgianf@microsoft.com>
2015-09-28 10:42:51 -0700

    Fix some small issues with C# examples.
     - Missing dispose of SqlCommand's
    
     - Fixing indentation to match published guidlines for C#.
    
     - Use one connection string.

--- C#/sample-.net-windows.cs
+++ C#/sample-.net-windows.cs
@@ -5,47 +5,54 @@ class Sample
 {
     static void Main()
     {
-      using(var conn = new SqlConnection("Server=tcp:yourserver.database.windows.net,1433;Database=yourdatabase;User ID=yourlogin@yourserver;Password={your_password_here};Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+        string connectionString = "Server=tcp:yourserver.database.windows.net,1433;Database=yourdatabase;User ID=yourlogin@yourserver;Password={your_password_here};Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        using (var conn = new SqlConnection(connectionString))
         {
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = @"
-                    SELECT 
-                        c.CustomerID
-                        ,c.CompanyName
-                        ,COUNT(soh.SalesOrderID) AS OrderCount
-                    FROM SalesLT.Customer AS c
-                    LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID
-                    GROUP BY c.CustomerID, c.CompanyName
-                    ORDER BY OrderCount DESC;";
-
-            conn.Open();    
-
-            using(var reader = cmd.ExecuteReader())
+            using (var cmd = conn.CreateCommand())
             {
-                while(reader.Read())
+                cmd.CommandText = @"
+                        SELECT
+                            c.CustomerID
+                            ,c.CompanyName
+                            ,COUNT(soh.SalesOrderID) AS OrderCount
+                        FROM SalesLT.Customer AS c
+                        LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID
+                        GROUP BY c.CustomerID, c.CompanyName
+                        ORDER BY OrderCount DESC;";
+
+                conn.Open();
+
+                using (var reader = cmd.ExecuteReader())
                 {
-                    Console.WriteLine("ID: {0} Name: {1} Order Count: {2}", reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2));
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("ID: {0} Name: {1} Order Count: {2}", reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2));
+                    }
                 }
-            }                   
+            }
         }
-         using(var conn = new SqlConnection("Server=tcp:yourserver.database.windows.net,1433;Database=yourdatabase;User ID=yourlogin@yourserver;Password={your_password_here};Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+
+        using (var conn = new SqlConnection(connectionString))
         {
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = @"
-                INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) 
-                OUTPUT INSERTED.ProductID
-                VALUES (@Name, @Number, @Cost, @Price, CURRENT_TIMESTAMP)";
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"
+                    INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate)
+                    OUTPUT INSERTED.ProductID
+                    VALUES (@Name, @Number, @Cost, @Price, CURRENT_TIMESTAMP)";
 
-            cmd.Parameters.AddWithValue("@Name", "SQL Server Express");
-            cmd.Parameters.AddWithValue("@Number", "SQLEXPRESS1");
-            cmd.Parameters.AddWithValue("@Cost", 0);
-            cmd.Parameters.AddWithValue("@Price", 0);
+                cmd.Parameters.AddWithValue("@Name", "SQL Server Express");
+                cmd.Parameters.AddWithValue("@Number", "SQLEXPRESS1");
+                cmd.Parameters.AddWithValue("@Cost", 0);
+                cmd.Parameters.AddWithValue("@Price", 0);
 
-            conn.Open();
+                conn.Open();
 
-            int insertedProductId = (int)cmd.ExecuteScalar();
+                int insertedProductId = (int)cmd.ExecuteScalar();
 
-            Console.WriteLine("Product ID {0} inserted.", insertedProductId);
+                Console.WriteLine("Product ID {0} inserted.", insertedProductId);
+            }
         }
     }
 }
