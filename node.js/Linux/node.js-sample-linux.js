f26a63ed891067bc34908215b322eed116b20792
Andrea Lam <andrela@microsoft.com>
2016-03-28 20:56:09 -0700

    Update node.js-sample-linux.js

--- node.js/Linux/node.js-sample-linux.js
+++ node.js/Linux/node.js-sample-linux.js
@@ -19,7 +19,7 @@ var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 
 function executeStatement() {
-    request = new Request("SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;", function(err) {
+    request = new Request("SELECT TOP 10 Title, FirstName, LastName from SalesLT.Customer;", function(err) {
     if (err) {
         console.log(err);} 
     });
